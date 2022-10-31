ef778dd8a8e048d72bd6501a77cdc0dce5c8db01
Adam Kaufman <adamkaufman@Adams-MacBook-Pro-3.local>
2022-10-31 16:25:07 -0400

    drop database

--- seed/data.js
+++ seed/data.js
@@ -4,8 +4,8 @@ import characters from "./characters.json" assert { type: "json" }
 
 const insertData = async () => {
   //reset database
-  // await db.dropDatabase()
-  await Character.deleteMany({})
+  await db.dropDatabase()
+  // await Character.deleteMany({})
   
   //insert data
   await Character.create(characters)
